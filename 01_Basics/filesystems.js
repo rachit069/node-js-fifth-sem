8d908685f36e5e3504e1fff873e360670d9929e7
rachit069 <122594011+rachit069@users.noreply.github.com>
2024-07-23 19:05:40 +0000

    day 2 learning node js

--- 01_Basics/filesystems.js
+++ 01_Basics/filesystems.js
@@ -1,9 +1,39 @@
 const fs = require("fs/promises")
 
-async function readFun()
-{
-    const file = await readFile("./demo.txt")
-    console.log(file)
-}
-readFun()
+// async function readFun()
+// {
+//     const file = await fs.readFile("/workspaces/node-js-fifth-sem/01_Basics/demo.txt")
+//     console.log(file)
+// }
+// readFun()
+
+// async function writeFun()
+// {
+//     const file = await fs.writeFile("/workspaces/node-js-fifth-sem/01_Basics/doc.txt","Hello guys, i am coding in node js")
+//     console.log(file)
+// }
+// writeFun()
+
+// async function copyFun()
+// {
+//     const file = await fs.copyFile("/workspaces/node-js-fifth-sem/01_Basics/doc.txt","/workspaces/node-js-fifth-sem/01_Basics/demo.txt")
+//     console.log(file)
+// }
+// copyFun()
+
+// async function deleteFun()
+// {
+//     const file = await fs.unlink("/workspaces/node-js-fifth-sem/01_Basics/doc.txt")
+//     console.log(file)
+// }
+// deleteFun()
+
+// async function addFun()
+// {
+//     const file = await fs.appendFile("/workspaces/node-js-fifth-sem/01_Basics/demo.txt","\tI am appending this file, ok!")
+//     console.log(file)
+// }
+// addFun()
+
+
 
