98ec616c80392062136b0ae75068ccc65015ce14
rachit069 <122594011+rachit069@users.noreply.github.com>
2024-07-25 15:55:57 +0000

    25th July

--- 02_Server/http.js
+++ 02_Server/http.js
@@ -1,18 +1,22 @@
 const http = require("http")
-
+const fs = require("fs")
 const server = http.createServer((request,response)=>{
-    response.writeHead(200,{"Content-type":"text/plain"})
-    const person = {
-        name: "rachit",
-        age: 10,
-        pnumber: 70181,
-        city: "rajpura"
-    }
-    // console.log(JSON.stringify(person))
-    response.end(JSON.stringify(person))
+    response.writeHead(200,{"Content-type":"text/html"})
+    // const person = {
+    //     name: "rachit",
+    //     age: 10,
+    //     pnumber: 70181,
+    //     city: "rajpura"
+    // }
+    // // console.log(JSON.stringify(person))
+    // response.end(JSON.stringify(person))
+    
+    // response.end(JSON.stringify(person))
+
+    const read = fs.createReadStream("/workspaces/node-js-fifth-sem/02_Server/index.html")
+    read.pipe(response)
 })
 
 server.listen(3000,()=>{
     console.log("server is starting on 3000 port")
 })
-
