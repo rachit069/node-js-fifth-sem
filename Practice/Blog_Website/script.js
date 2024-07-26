055751bd5da0b0185aa73b5236196f4e64aa03de
rachit069 <122594011+rachit069@users.noreply.github.com>
2024-07-26 09:01:34 +0000

    blog site deployed on server

---
+++ Practice/Blog_Website/script.js
@@ -0,0 +1,31 @@
+const http = require("http")
+const fs = require("fs")
+const server = http.createServer((request,response)=>{
+
+    console.log(request.url)
+    const {url} = request;
+    if(url=="/"){
+        response.writeHead(200,{"Content-type":"text/html"})
+        const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Blog_Website/landing.html")
+        read.pipe(response)
+    }
+    else if(url=="/login"){
+    response.writeHead(200,{"Content-type":"text/html"})
+    const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Blog_Website/login.html")
+    read.pipe(response)
+    }
+    else if(url=="/style.css"){
+        response.writeHead(200,{"Content-type":"text/css"})
+        const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Blog_Website/style.css")
+        read.pipe(response)
+        }
+    else{
+        response.writeHead(200,{"Content-type":"text/html"})
+    const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Blog_Website/error.html")
+    read.pipe(response)
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("server is starting on 3000 port")
+})
