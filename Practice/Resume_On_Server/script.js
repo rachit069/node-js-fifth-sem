f16feeec081316c8d1d43536d3c5b6d77221a47f
rachit069 <122594011+rachit069@users.noreply.github.com>
2024-07-26 08:16:23 +0000

    third commit

--- Practice/Resume_On_Server/script.js
+++ Practice/Resume_On_Server/script.js
@@ -1,9 +1,19 @@
 const http = require("http")
 const fs = require("fs")
 const server = http.createServer((request,response)=>{
+
+    console.log(request.url)
+    const {url} = request;
+    if(url=="/"){
+        response.writeHead(200,{"Content-type":"text/html"})
+        const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Resume_On_Server/new1.html")
+        read.pipe(response)
+    }
+    else if(url=="/newurl"){
     response.writeHead(200,{"Content-type":"text/html"})
-    const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Resume_On_Server/index.html")
+    const read = fs.createReadStream("/workspaces/node-js-fifth-sem/Practice/Resume_On_Server/new.html")
     read.pipe(response)
+    }
 })
 
 server.listen(3000,()=>{
